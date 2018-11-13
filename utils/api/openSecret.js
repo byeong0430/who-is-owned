9e20731995a07b8b45e496797f3a25db2e8ee91a
byeong0430 <byeong.kim0430@gmail.com>
2018-11-13 10:01:34 -0800

    Add api key in .env. Get a new api key after this.

--- utils/api/openSecret.js
+++ utils/api/openSecret.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
+import { API_KEY } from 'react-native-dotenv';
 
 const baseUrl = 'http://www.opensecrets.org/api/';
-const apiKey = 'cf7a38e0bc76b9261aa78102ce8de402';
-export const getLegislators = async stateCode => {
-  const url = `${baseUrl}/?method=getLegislators&id=${stateCode}&apikey=${apiKey}`;
 
+export const getLegislators = async stateCode => {
   const response = await axios.get(baseUrl, {
     timeout: 6000,
     params: {
       method: 'getLegislators',
       id: stateCode,
-      apikey: apiKey
+      apikey: API_KEY
     }
   });
 
-  if (response.status !== 200){
+  if (response.status !== 200) {
     throw new Error('Something went wrong..\nPlease try again later.');
   }
 
