31cc807bbc155ee8de2b1c9e387cc8725f4599a4
Byeong Kim <byeong.kim0430@gmail.com>
2018-11-13 20:30:45 -0800

    Add configParams() to add extra parameters into default params Generalise getLegislator()

--- utils/api/openSecret.js
+++ utils/api/openSecret.js
@@ -1,21 +1,30 @@
 import axios from 'axios';
 import { API_KEY } from 'react-native-dotenv';
 
-const baseUrl = 'http://www.opensecrets.org/api/';
-
-export const getLegislators = async paramArgs => {
-  const { verb } = paramArgs;
-  delete paramArgs['verb'];
-
-  const result = await axios.get(baseUrl, {
+const configParams = params => {
+  const defaultParams = {
     timeout: 6000,
-    method: verb,
     params: {
       apikey: API_KEY,
-      output: 'json',
-      ...paramArgs
+      output: 'json'
     }
-  });
+  };
+
+  // Add HTTP verb and delete it from params
+  defaultParams.method = params.verb;
+  delete params['verb'];
+
+  // Add the rest of the key-value pairs to defaultParams.params
+  defaultParams.params = { ...defaultParams.params, ...params }
+
+  return defaultParams;
+};
+
+export const fetchData = async paramArgs => {
+  const result = await axios.get(
+    'http://www.opensecrets.org/api/',
+    configParams(paramArgs)
+    );
 
   if (result.status !== 200) {
     throw new Error('Something went wrong..\nPlease try again later.');
