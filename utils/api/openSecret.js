5f060d83cd8774473d601d17cc13577fa1cf6956
Byeong Kim <byeong.kim0430@gmail.com>
2018-11-13 20:27:40 -0800

    Use dotenv npm package to store API_KEY Consolidate input of getLegislators as `paramArgs`

--- utils/api/openSecret.js
+++ utils/api/openSecret.js
@@ -3,19 +3,23 @@ import { API_KEY } from 'react-native-dotenv';
 
 const baseUrl = 'http://www.opensecrets.org/api/';
 
-export const getLegislators = async stateCode => {
-  const response = await axios.get(baseUrl, {
+export const getLegislators = async paramArgs => {
+  const { verb } = paramArgs;
+  delete paramArgs['verb'];
+
+  const result = await axios.get(baseUrl, {
     timeout: 6000,
+    method: verb,
     params: {
-      method: 'getLegislators',
-      id: stateCode,
-      apikey: API_KEY
+      apikey: API_KEY,
+      output: 'json',
+      ...paramArgs
     }
   });
 
-  if (response.status !== 200) {
+  if (result.status !== 200) {
     throw new Error('Something went wrong..\nPlease try again later.');
   }
 
-  return response.data;
+  return result.data.response;
 };
