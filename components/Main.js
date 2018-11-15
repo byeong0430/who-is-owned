b3d9eafba66d970d47de868535eb3a59551e2a0a
Byeong Kim <byeong.kim0430@gmail.com>
2018-11-14 18:53:10 -0800

    Add socialIcon and render the icons

--- components/Main.js
+++ components/Main.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, ScrollView, Text } from 'react-native';
 import * as theme from '../utils/theme';
 import * as openSecret from '../utils/api/openSecret';
-import { Icon } from 'react-native-elements';
+import { Icon, SocialIcon } from 'react-native-elements';
 import { Ionicons } from '@expo/vector-icons';
 
 export default class Main extends Component {
@@ -25,6 +25,25 @@ export default class Main extends Component {
       });
       this.setState({ apiResults });
     }
+  }
+
+  renderSocialIconBtns = item => {
+    const socialMedias = ['facebook_id', 'twitter_id', 'youtube_url'];
+
+    return socialMedias.map(socialMedia => {
+      const socialMediaType = socialMedia.split('_')[0];
+      if (item[socialMedia]) {
+        return (
+          <SocialIcon
+            key={socialMediaType}
+            raised={true}
+            type={socialMediaType}
+            style={theme.socialIcon}
+            onPress={() => { console.log('test') }}
+          />
+        );
+      }
+    })
   }
 
   renderFields = (key, items) => {
@@ -51,18 +70,7 @@ export default class Main extends Component {
               </View>
               {/* Footer */}
               <View style={theme.profileFooter}>
-                <Ionicons
-                  name="logo-facebook"
-                  size={32} color="blue"
-                />
-                <Ionicons
-                  name="logo-twitter"
-                  size={32} color="blue"
-                />
-                <Ionicons
-                  name="logo-youtube"
-                  size={32} color='red'
-                />
+                {this.renderSocialIconBtns(item['@attributes'])}
               </View>
             </View>
           );
