096c8ae2d7078836209b32c090242fcb0c7b85e7
Byeong Kim <byeong.kim0430@gmail.com>
2018-11-15 23:23:08 -0800

    split theme.js stylesheet into multiple files

---
+++ utils/stylesheets/header.js
@@ -0,0 +1,18 @@
+// Header
+export const headerContainer = {
+  marginBottom: 20,
+  borderTopWidth: 0,
+  marginTop: 0,
+  marginBottom: 0
+};
+
+// Reference
+// https://react-native-training.github.io/react-native-elements/docs/0.19.1/lists.html#lefticon
+export const headerIcon = {
+  name: 'edit-location',
+  type: 'material',
+  style: {
+    fontSize: 50,
+    borderColor: 'transparent'
+  }
+}
