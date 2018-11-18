5ca029571066457ff5b1882879bdb82d0fa5bea2
Byeong Kim <byeong.kim0430@gmail.com>
2018-11-17 22:11:02 -0800

    Load new font

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
 
   componentDidMount = async () => {
     await Font.loadAsync({
-      'slabo': require('./assets/fonts/Merriweather-Regular.ttf')
+      'fjallaRegular': require('./assets/fonts/FjallaOne-Regular.ttf')
     });
 
     this.setState({ fontLoaded: true });
