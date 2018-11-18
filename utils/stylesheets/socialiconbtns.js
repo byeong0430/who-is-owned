63a7cfd1e27128a5c3461962be8ba8b6b036c040
Byeong Kim <byeong.kim0430@gmail.com>
2018-11-18 13:30:15 -0800

    Right-align icons and reduce size

--- utils/stylesheets/socialiconbtns.js
+++ utils/stylesheets/socialiconbtns.js
@@ -2,10 +2,13 @@ import * as vars from './vars';
 
 export const profileFooter = {
   ...vars.debugBorder,
-  flexDirection: 'row'
+  flexDirection: 'row',
+  justifyContent: 'flex-end'
 }
 
 export const socialIcon = {
   width: vars.mediumIconSize,
   height: vars.mediumIconSize
 }
+
+export const iconSize = vars.mediumFontSize;
