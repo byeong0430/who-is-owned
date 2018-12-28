ec004fb6b66fbfb9fc7d0f376d5d9328e6774eb2
Byeong Kim <byeong.kim0430@gmail.com>
2018-12-28 15:28:52 -0800

    Add styling to sidemenu

--- utils/stylesheets/sidemenu.js
+++ utils/stylesheets/sidemenu.js
@@ -1,3 +1,5 @@
+import * as vars from './vars';
+
 // SideMenu
 export const sideMenuBackBtn = {
   marginLeft: 15
@@ -8,5 +10,19 @@ export const sideMenuContainer = {
   alignItems: 'center',
   justifyContent: 'center',
   borderColor: 'blue',
-  borderWidth: 1
+  borderWidth: 1,
+  marginTop: 80,
+  ...vars.debugBorder
 };
+
+export const sideMenuInput = {
+  alignSelf: 'stretch',
+  padding: 8,
+  borderColor: 'gray',
+  borderWidth: 1
+}
+
+export const sideMenuResult = {
+  margin: 8,
+  color: 'transparent'
+}
