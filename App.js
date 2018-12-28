e49e09ad5a469967cba0c781f13ea1c97a882ee2
Byeong Kim <byeong.kim0430@gmail.com>
2018-12-28 15:26:46 -0800

    Re-locate functions from Home.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Font } from 'expo';
+import { Platform } from 'react-native';
+import { Constants, Location, Permissions, Font } from 'expo';
+const usStateCode = require('./utils/data/usStateCode.json');
 
 // Navigation config
 import { RootStack } from './utils/navigation/RootStack';
@@ -8,7 +10,47 @@ import { RootStack } from './utils/navigation/RootStack';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { fontLoaded: false };
+    this.state = {
+      fontLoaded: false,
+      iniGps: null,
+      location: null,
+      errorMessage: null
+    };
+  }
+
+  componentWillMount() {
+    // Constants.isDevice === true if you're using an emulator
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'This will not work on Sketch in an Android emulator. Try it on your device!'
+      })
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    // Ask for location permission
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied!'
+      })
+    }
+
+    // Get GPS info
+    const iniGps = await Location.getCurrentPositionAsync({});
+    this.setState({ iniGps });
+
+    // Parse longitude and latitude and get current address
+    let { longitude, latitude } = iniGps.coords;
+
+    // Debugging with initial location set to L.A.
+    longitude = -118.2437;
+    latitude = 34.0522;
+
+    this.updateLoc(latitude, longitude);
   }
 
   componentDidMount = async () => {
@@ -18,9 +60,28 @@ export default class App extends Component {
 
     this.setState({ fontLoaded: true });
   }
+
+  updateLoc = async (latitude, longitude) => {
+    let location = await Location.reverseGeocodeAsync({ longitude, latitude });
+
+    // Select the first array item
+    location = location[0];
+
+    if (location.country === 'United States') {
+      location.regionCode = this.getUsRegionCode(location.region, usStateCode);
+    }
+
+    this.setState({ location });
+  }
+
+  getUsRegionCode = (state, stateCodeJson) => stateCodeJson[state];
+
   render() {
     return (
-      this.state.fontLoaded ? <RootStack /> : null
+      this.state.fontLoaded ? <RootStack screenProps={{
+        updateLoc: this.updateLoc,
+        location: this.state.location
+      }} /> : null
     );
   }
 }
