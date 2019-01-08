33bf9470db75a7b8f6225adbc23778a977686354
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-07 23:24:41 -0800

    Specify `apiResults` to `legiSummary`

--- components/home/Main.js
+++ components/home/Main.js
@@ -64,7 +64,7 @@ export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      apiResults: null
+      legiSummary: null
     };
   }
 
@@ -73,12 +73,17 @@ export default class Main extends Component {
     if (location &&
       (this.props.location !== prevProps.location) ||
       (this.props.method !== prevProps.method)) {
-      const apiResults = await openSecret.fetchData({
+
+      let legiSummary = await openSecret.fetchData({
         verb: 'GET',
         method,
         id: location.regionCode
       });
-      this.setState({ apiResults });
+      legiSummary = legiSummary.legislator;
+
+      this.setState({ legiSummary });
+
+
     }
   }
 
@@ -102,9 +107,9 @@ export default class Main extends Component {
     })
   }
 
-  renderFields = (key, items) => {
+  renderFields = items => {
     if (items) {
-      return items[key].map((item, index) => {
+      return items.map((item, index) => {
         if (index === 0 || index === 1) {
           return (
             <View key={`view_${index}`} style={mainStyle.profileContainer}>
@@ -126,8 +131,8 @@ export default class Main extends Component {
   render() {
     return (
       <ScrollView style={mainStyle.mainContainer}>
-        {this.renderFields('legislator', this.state.apiResults)}
-        {/* {this.renderFields('legislator', examples)} */}
+        {this.renderFields(this.state.legiSummary)}
+        {/* {this.renderFields(examples)} */}
       </ScrollView>
     )
   }
