a80027e5bc6630ddfb09b6836af54f05efd09103
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-08 22:12:37 -0800

    Add scrollview and capitalize icon titles

--- components/profile/SocialIconBtns.js
+++ components/profile/SocialIconBtns.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Linking } from 'react-native';
+import { Text, View, ScrollView, Linking } from 'react-native';
 import { SocialIcon, Icon } from 'react-native-elements';
 import * as socialIconBtnStyle from '../../utils/stylesheets/socialiconbtns';
 
@@ -26,16 +26,21 @@ export default class ProfileHeader extends Component {
       const socialMediaType = socialMedia.split('_')[0];
       if (item[socialMedia]) {
         return (
-          <SocialIcon
-            key={socialMediaType}
-            raised={true}
-            type={socialMediaType}
-            style={socialIconBtnStyle.socialIcon}
-            iconSize={socialIconBtnStyle.iconSize}
-            onPress={() => (
-              Linking.openURL(this.makeSocialMediaUrl(item, socialMedia))
-            )}
-          />
+          <View style={socialIconBtnStyle.iconBox}>
+            <SocialIcon
+              key={socialMediaType}
+              raised={true}
+              type={socialMediaType}
+              style={socialIconBtnStyle.socialIcon}
+              iconSize={socialIconBtnStyle.iconSize}
+              onPress={() => (
+                Linking.openURL(this.makeSocialMediaUrl(item, socialMedia))
+              )}
+            />
+            <Text style={socialIconBtnStyle.iconTitle}>
+              {socialMediaType.replace(/^[a-z]/, str => str.toUpperCase())}
+            </Text>
+          </View>
         );
       }
     })
@@ -45,9 +50,12 @@ export default class ProfileHeader extends Component {
     const { attributes } = this.props;
 
     return (
-      <View style={{ marginLeft: 13, marginTop: 10 }}>
-        <Text>Connect</Text>
-        <View style={socialIconBtnStyle.profileFooter}>
+      <View style={socialIconBtnStyle.profileFooter}>
+        <Text>CONNECT</Text>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        >
           {this.renderSocialIconBtns(attributes)}
           <Icon
             name='phone-in-talk'
@@ -56,7 +64,7 @@ export default class ProfileHeader extends Component {
               Linking.openURL(`tel:1 ${attributes.phone}`)
             )}
           />
-        </View>
+        </ScrollView>
       </View>
     );
   }
