c97dc5d06880a66c027e814ef5f0a826e5b453fc
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-08 17:24:31 -0800

    Add ProfileMain component

---
+++ components/profile/ProfileMain.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+export default class ProfileMain extends Component {
+  formatDate = strDate => {
+    const date = new Date(strDate);
+    const noDay = date.toDateString().replace(/^[a-zA-Z]+\s/g, '');
+    return noDay.replace(/\d+\s\d+/g, numStr => {
+      return numStr.replace(' ', ', ');
+    })
+  }
+
+  calculateAge = (strDate, delimeter) => {
+    const y = parseInt(strDate.split(delimeter));
+    const now = new Date().getFullYear();
+    return now - y;
+  }
+
+  render() {
+    const { birthdate, congress_office } = this.props.attributes;
+
+    return (
+      <View>
+        <ListItem
+          hideChevron
+          leftIcon={{ name: 'person', color: 'grey' }}
+          containerStyle={{ borderBottomWidth: 0 }}
+          title='Born'
+          subtitle={`${this.formatDate(birthdate)} (${this.calculateAge(birthdate, '-')} years of age)`}
+        />
+        <ListItem
+          hideChevron
+          leftIcon={{ name: 'work', color: 'grey' }}
+          title='Congress Office'
+          subtitle={congress_office}
+        />
+      </View>
+    );
+  }
+}
