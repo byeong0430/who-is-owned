103c9f1c3a4410c92af43a5392ee333cf46cb6c1
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-08 17:25:11 -0800

    Link icons to url

--- components/profile/SocialIconBtns.js
+++ components/profile/SocialIconBtns.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { SocialIcon } from 'react-native-elements';
+import { Text, View, Linking } from 'react-native';
+import { SocialIcon, Icon } from 'react-native-elements';
 import * as socialIconBtnStyle from '../../utils/stylesheets/socialiconbtns';
 
 export default class ProfileHeader extends Component {
+  makeSocialMediaUrl = (smItems, smType) => {
+    const socialMediaType = smType.split('_')[0];
+    let url = '';
+
+    switch (smType) {
+      case 'youtube_url':
+        url = smItems[smType];
+        break;
+      default:
+        url = `https://www.${socialMediaType}.com/${smItems[smType]}`
+    }
+
+    return url;
+  }
+
   renderSocialIconBtns = item => {
     const socialMedias = ['facebook_id', 'twitter_id', 'youtube_url'];
 
     return socialMedias.map(socialMedia => {
       const socialMediaType = socialMedia.split('_')[0];
-
       if (item[socialMedia]) {
         return (
           <SocialIcon
@@ -18,7 +32,9 @@ export default class ProfileHeader extends Component {
             type={socialMediaType}
             style={socialIconBtnStyle.socialIcon}
             iconSize={socialIconBtnStyle.iconSize}
-            onPress={() => { console.log('test') }}
+            onPress={() => (
+              Linking.openURL(this.makeSocialMediaUrl(item, socialMedia))
+            )}
           />
         );
       }
@@ -26,9 +42,21 @@ export default class ProfileHeader extends Component {
   }
 
   render() {
+    const { attributes } = this.props;
+
     return (
-      <View style={socialIconBtnStyle.profileFooter}>
-        {this.renderSocialIconBtns(this.props.attributes)}
+      <View style={{ marginLeft: 13, marginTop: 10 }}>
+        <Text>Connect</Text>
+        <View style={socialIconBtnStyle.profileFooter}>
+          {this.renderSocialIconBtns(attributes)}
+          <Icon
+            name='phone-in-talk'
+            onPress={() => (
+              // Assume US (+1) for now
+              Linking.openURL(`tel:1 ${attributes.phone}`)
+            )}
+          />
+        </View>
       </View>
     );
   }
