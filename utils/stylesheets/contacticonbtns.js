b317609e976d441f291d6b793eceb36a409ee2e1
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-08 22:50:38 -0800

    Add ContactIconBtns component and stylesheet

---
+++ utils/stylesheets/contacticonbtns.js
@@ -0,0 +1,20 @@
+import * as vars from './vars';
+
+export const iconBox = {
+  flex: 1,
+  alignItems: 'center',
+  width: 65
+}
+
+export const footerIcon = {
+  width: vars.mediumIconSize,
+  height: vars.mediumIconSize
+}
+
+export const iconTitle = {
+  textAlign: 'center',
+  color: 'grey',
+  fontSize: vars.smallFontSize
+}
+
+export const iconSize = vars.mediumFontSize;
