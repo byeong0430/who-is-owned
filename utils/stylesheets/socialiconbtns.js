a7c72d481e15827756677fa6dccb6cda69aded61
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-08 22:52:44 -0800

    Adjust style for social icon btns

--- utils/stylesheets/socialiconbtns.js
+++ utils/stylesheets/socialiconbtns.js
@@ -2,23 +2,25 @@ import * as vars from './vars';
 
 export const profileFooter = {
   marginLeft: 13,
-  marginTop: 10
+  marginTop: 10,
+  marginBottom: 10
 }
 
 export const iconBox = {
   flex: 1,
   alignItems: 'center',
-  marginRight: 10
+  width: 65
 }
 
-export const socialIcon = {
+export const footerIcon = {
   width: vars.mediumIconSize,
   height: vars.mediumIconSize
 }
 
 export const iconTitle = {
   textAlign: 'center',
-  color: 'grey'
+  color: 'grey',
+  fontSize: vars.smallFontSize
 }
 
 export const iconSize = vars.mediumFontSize;
