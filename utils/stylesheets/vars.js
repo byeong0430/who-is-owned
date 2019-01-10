fdb21026bd3f7e5083e60839dd2f0983efd016ce
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-09 22:32:03 -0800

    Add more theme colours

--- utils/stylesheets/vars.js
+++ utils/stylesheets/vars.js
@@ -4,11 +4,20 @@ import { Platform } from 'react-native';
 // 5 levels of blue colours from lightest to darkest
 // Reference: http://paletton.com/#uid=13F0u0kvJEBjEKwpjGCE2tqFFmS
 export const themeColors = ['#5D8CD2','#3872C8','#135AC2','#0A449B','#073578'];
-export const redColor = '#C02F1D';
-export const mainFont = 'fjallaRegular';
+export const redColor = '#b9110f';
+export const yellowColor = '#ffaf00';
+export const blueColor = '#0091cf';
+export const darkBlueColor = '#01395e';
+export const darkGreyColor = '#343239';
+export const greyColor = '#666564';
+export const lightGreyColor = '#e3e1e3';
+
+export const mainFont = 'RobotoRegular';
+
 export const bigIconSize = 40;
 export const mediumIconSize = 30;
 export const smallIconSize = 22;
+
 export const bigFontSize = 22;
 export const mediumFontSize = 18;
 export const smallFontSize = 13;
