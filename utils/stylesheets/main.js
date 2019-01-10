78cf7e1bea74b593c2d602a4e7824b9bc327f323
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-09 22:43:55 -0800

    Remove unused style

--- utils/stylesheets/main.js
+++ utils/stylesheets/main.js
@@ -1,19 +1,5 @@
 import * as vars from './vars';
 
-// Main
-export const mainContainer = {
-  marginLeft: 5,
-  marginRight: 5
-}
-
-export const profileContainer = {
-  paddingTop: 5,
-  paddingLeft: 5,
-  paddingRight: 5,
-  paddingBottom: 5,
-  marginBottom: 10,
-}
-
 export const card = {
   padding: 0,
   borderRadius: 5
