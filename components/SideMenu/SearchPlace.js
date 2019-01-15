adee06877e66391e941c29b0d679afb36d9e62ed
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-15 15:15:01 -0800

    Add dispatcher

--- components/SideMenu/SearchPlace.js
+++ components/SideMenu/SearchPlace.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 import * as strFunc from '../../utils/functions/strFunctions';
+import { handleGetLocation } from '../../utils/functions/locFunctions';
+import appReducer from '../../reducers/reducer';
+import { createStore } from 'redux';
 import * as sidemenuStyle from '../../utils/stylesheets/sidemenu';
 
+const store = createStore(appReducer);
+
 export default class SearchPlace extends Component {
-  handleUpdateAndOpenHome = (lat, lng) => {
+  handleUpdateAndOpenHome = async (longitude, latitude) => {
     // Update location
-    this.props.updateLoc(lat, lng);
+    const location = await handleGetLocation(longitude, latitude);
+
+    store.dispatch({
+      type: 'UPDATE_LOCATION',
+      payload: { location }
+    })
+
     this.props.navigation.navigate('Home');
   }
 
@@ -15,13 +26,14 @@ export default class SearchPlace extends Component {
       locale_names, city, county, country,
       _geoloc: { lat, lng }
     } = this.props.item;
+
     const address = strFunc.joinArrayStr(
       [locale_names, city, county, country], ', '
     );
 
     return (
       <TouchableOpacity
-        onPress={() => this.handleUpdateAndOpenHome(lat, lng)}
+        onPress={() => this.handleUpdateAndOpenHome(lng, lat)}
         style={sidemenuStyle.sideMenuResult}
         underlayColor='white'
       >
