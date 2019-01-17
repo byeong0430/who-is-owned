94e0c1f215ffa4de4c6b7aba99105bf0d2b594e2
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-17 11:47:54 -0800

    Move loading-related stuff to LoadingBackground component

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
-import { Constants, Location, Permissions, Font } from 'expo';
+import { Font } from 'expo';
 import React, { Component } from 'react';
-import { Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import reduxThunk from 'redux-thunk'; // Middleware handler
 import rootReducer from './redux/reducers/';
-import { handleInitialGpsLocUpdate } from './redux/thunks';
+import LoadingBackground from './Screens/LoadingBackground';
 
 // Navigation config
 import { RootStack } from './utils/navigation/RootStack';
 
 // Create a store
-const store = createStore(rootReducer, applyMiddleware(logger, reduxThunk));
+const store = createStore(rootReducer, applyMiddleware(reduxThunk));
 
 // You can have an option to add a loading page
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fontLoaded: false,
-      errorMessage: null
+      fontLoaded: false
     };
   }
 
@@ -33,47 +31,15 @@ export default class App extends Component {
     this.setState({ fontLoaded: true });
   }
 
-  componentWillMount = async () => {
-    // Constants.isDevice === true if you're using an emulator
-    if (Platform.OS === 'android' && !Constants.isDevice) {
-      this.setState({
-        errorMessage: 'This will not work on Sketch in an Android emulator. Try it on your device!'
-      })
-    } else {
-      const gps = await this.handleGetGps();
-
-      // Parse longitude and latitude and get current address
-      let { longitude, latitude } = gps.coords;
-
-      // Debugging with initial location set to L.A.
-      longitude = -118.2437;
-      latitude = 34.0522;
-
-      store.dispatch(handleInitialGpsLocUpdate(longitude, latitude));
-    }
-  }
-
-  handleGetGps = async () => {
-    // Ask for location permission
-    const { status } = await Permissions.askAsync(Permissions.LOCATION);
-
-    if (status !== 'granted') {
-      this.setState({
-        errorMessage: 'Permission to access location was denied!'
-      })
-    }
-
-    // Get GPS info
-    return await Location.getCurrentPositionAsync({});
-  }
-
   render() {
     return (
-      this.state.fontLoaded && (
-        <Provider store={store}>
-          <RootStack />
-        </Provider>
-      )
+      <Provider store={store}>
+        {
+          this.state.fontLoaded
+            ? <RootStack />
+            : <LoadingBackground />
+        }
+      </Provider>
     );
   }
 }
