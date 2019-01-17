b4e9efaf56f2c6d0b218eee8bfcaeba230f4b6da
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-16 16:40:25 -0800

    Add middleware for sidemenu

--- redux/thunks.js
+++ redux/thunks.js
@@ -1,8 +1,22 @@
 import { updateGps, updateLocation } from './actions/locActions';
+import { updateQuery, updateHits } from './actions/sideMenuActions';
 import { handleGetLocation } from '../utils/functions/locFunctions';
 
-export const updateGpsAndLocation = (longitude, latitude) => async dispatch => {
+import AlgoliaPlace from '../utils/api/AlgoliaPlace';
+
+// algolia places api: https://community.algolia.com/places/api-clients.html
+const ap = new AlgoliaPlace();
+
+export const handleUpdateGpsAndLocation = (longitude, latitude) => async dispatch => {
   dispatch(updateGps(longitude, latitude));
   const location = await handleGetLocation(longitude, latitude);
   dispatch(updateLocation(location));
+};
+
+export const handleLoadPlaces = (query, gps) => async dispatch => {
+  dispatch(updateQuery(query));
+  const aroundLatLng = gps ? `${gps.latitude},${gps.longitude}` : undefined;
+  const hits = await ap.getPlaces({ query, aroundLatLng });
+
+  dispatch(updateHits(hits));
 };
