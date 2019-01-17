ab427e6f04263f53b0390123cb28271f9ff1ce09
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-16 17:29:12 -0800

    Add render legislator card function

--- redux/thunks.js
+++ redux/thunks.js
@@ -1,11 +1,14 @@
 import { updateGps, updateLocation } from './actions/locActions';
 import { updateQuery, updateHits } from './actions/sideMenuActions';
+import { updateLegislators } from './actions/openSecretActions';
 import { handleGetLocation } from '../utils/functions/locFunctions';
 
+import OpenSecret from '../utils/api/OpenSecret';
 import AlgoliaPlace from '../utils/api/AlgoliaPlace';
 
 // algolia places api: https://community.algolia.com/places/api-clients.html
-const ap = new AlgoliaPlace();
+const algoliaPlace = new AlgoliaPlace();
+const openSecret = new OpenSecret();
 
 export const handleInitialGpsLocUpdate = (longitude, latitude) => async dispatch => {
   dispatch(updateGps(longitude, latitude));
@@ -23,7 +26,13 @@ export const handleGpsLocUpdateAndHome = (longitude, latitude, navigation) => as
 export const handleLoadPlaces = (query, gps) => async dispatch => {
   dispatch(updateQuery(query));
   const aroundLatLng = gps ? `${gps.latitude},${gps.longitude}` : undefined;
-  const hits = await ap.getPlaces({ query, aroundLatLng });
-
+  const hits = await algoliaPlace.getPlaces({ query, aroundLatLng });
   dispatch(updateHits(hits));
+};
+
+export const handleGetLegislators = regionCode => async dispatch => {
+  const legislatorSummary = await openSecret.getLegislators({
+    id: regionCode
+  });
+  dispatch(updateLegislators(legislatorSummary));
 };
