2cfe50126b1501601a605522b43a963e0389d54f
Byeong Kim <byeong.kim0430@gmail.com>
2019-01-17 11:45:24 -0800

    Add style objects for sidemenu

--- utils/stylesheets/sidemenu.js
+++ utils/stylesheets/sidemenu.js
@@ -9,20 +9,37 @@ export const sideMenuContainer = {
   flex: 1,
   alignItems: 'center',
   justifyContent: 'center',
-  borderColor: 'blue',
-  borderWidth: 1,
-  marginTop: 80,
-  ...vars.debugBorder
+  marginTop: 80
 };
 
 export const sideMenuInput = {
   alignSelf: 'stretch',
   padding: 8,
-  borderColor: 'gray',
-  borderWidth: 1
+  color: 'white',
+  backgroundColor: vars.blueColor
+}
+
+export const sideMenuScrollView = {
+  alignSelf: 'stretch'
+};
+
+export const sideMenuList = {
+  borderWidth: 0,
+  marginTop: 0
 }
 
 export const sideMenuResult = {
   margin: 8,
   color: 'transparent'
 }
+
+export const sideMenuListItemIcon = {
+  name: 'location-on',
+  type: 'material',
+  color: vars.blueColor,
+  size: vars.mediumIconSize
+};
+
+export const sideMenuListItem = {
+  flex: 1
+};
